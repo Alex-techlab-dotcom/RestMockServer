b638feb1eda1ee0ad84266f29b206aeb88d78693
SourceCodeWizard <chalassproduction@gmail.com>
2024-09-26 05:33:21 +1200

    Initial commit.

---
+++ src/main/java/source/code/wizzard/restmockserver/RestMockServerApplication.java
@@ -0,0 +1,13 @@
+package source.code.wizzard.restmockserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMockServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestMockServerApplication.class, args);
+    }
+
+}
